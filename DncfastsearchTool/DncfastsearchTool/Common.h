f461c01c6defa246a2098ce2a01065ea6447d754
UchihaMadara1 <2898831413@qq.com>
2020-04-24 12:00:26 +0800

    “文档搜索神器”

--- DncfastsearchTool/DncfastsearchTool/Common.h
+++ DncfastsearchTool/DncfastsearchTool/Common.h
@@ -9,10 +9,11 @@
 #include<thread>
 #include<chrono>
 #include<windows.h>
+#include<algorithm>
 
 #include "sqlite/sqlite3.h"
 using namespace std;
 
 
 #define SQL_BUFFER_SIZE 256
-#define MAX_TITLE_BUFFER 128
+#define MAX_TITLE_SIZE 128
--- DncfastsearchTool/DncfastsearchTool/Sysframe.h
+++ DncfastsearchTool/DncfastsearchTool/Sysframe.h
@@ -2,10 +2,19 @@
 
 #include "Common.h"
 
-void SetCurPos(int x, int y); // x row y col
+//void SetCurPos(int x, int y); // x row y col
+//
+//void DrawRow(int x,int y);
+//void DrawCol(int x,int y);
+//
+//void DrawFrame(char *title);
+//void Drawmenu();
 
-void DrawRow(int x,int y);
-void DrawCol(int x,int y);
+void SetCurPos(int x, int y);
+void HideCursor();
+void DrawCol(int x, int y);
+void DrawRow(int x, int y);
 
 void DrawFrame(char *title);
-void Drawmenu();
+void DrawMenu();
+void SystemEnd();
